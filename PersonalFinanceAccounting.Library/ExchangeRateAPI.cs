bb3e0c292cdb831675991881643427bc8a64d768
Анкушин Алексей <r141d@mail.ru>
2023-01-12 20:10:43 +0500

    внес изменения в класс ExchangeRateAPI с добавлением автодокументирования

--- PersonalFinanceAccounting.Library/ExchangeRateAPI.cs
+++ PersonalFinanceAccounting.Library/ExchangeRateAPI.cs
@@ -1,43 +1,43 @@
+using System.Globalization;
 using System.Xml;
 
 namespace PersonalFinanceAccounting.Library
 {
+    /// <summary>
+    /// Класс извлечения курса вавлют из API центрабанка
+    /// </summary>
     public static class ExchangeRateAPI
     {
+        /// <summary>
+        /// Метод получения курса валюты из API центрабанка по ID валюты
+        /// </summary>
+        /// <param name="ID">ID валюты</param>
+        /// <returns>(Decimal) коэфициент курса валюты по отношению к рублю</returns>
         public static decimal ExchangeRate(string ID) //"R01235" USD; "R01239" EUR
         {
             var reader = new XmlTextReader("http://www.cbr.ru/scripts/XML_daily.asp");
             var ExchangeXml = "";
             while (reader.Read())
             {
-                switch (reader.NodeType)
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "Valute")
                 {
-                    case XmlNodeType.Element:
-
-                        if (reader.Name == "Valute")
+                    if (reader.HasAttributes)
+                    {
+                        while (reader.MoveToNextAttribute())
                         {
-                            if (reader.HasAttributes)
+                            if (reader.Name == "ID" && reader.Value == ID)
                             {
-                                while (reader.MoveToNextAttribute())
-                                {
-                                    if (reader.Name == "ID")
-                                    {
-                                        if (reader.Value == ID)
-                                        {
-                                            reader.MoveToElement();
-                                            ExchangeXml = reader.ReadOuterXml();
-                                        }
-                                    }
-                                }
+                                _ = reader.MoveToElement();
+                                ExchangeXml = reader.ReadOuterXml();
                             }
                         }
-                        break;
+                    }
                 }
             }
-            XmlDocument XmlDocument = new XmlDocument();
+            var XmlDocument = new XmlDocument();
             XmlDocument.LoadXml(ExchangeXml);
-            XmlNode xmlNode = XmlDocument.SelectSingleNode("Valute/Value");
-            decimal rate = Convert.ToDecimal(xmlNode.InnerText);
+            var xmlNode = XmlDocument.SelectSingleNode("Valute/Value");
+            var rate = Convert.ToDecimal(xmlNode.InnerText, new CultureInfo("ru-RU"));
             return rate;
         }
     }
