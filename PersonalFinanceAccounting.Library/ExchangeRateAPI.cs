bcd8d42332994f031318fe090317f82ff32bddb0
Анкушин Алексей <r141d@mail.ru>
2023-01-12 18:20:29 +0500

    Создал класс запроса Курса валют

--- PersonalFinanceAccounting.Library/ExchangeRateAPI.cs
+++ PersonalFinanceAccounting.Library/ExchangeRateAPI.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
 
 namespace PersonalFinanceAccounting.Library
 {
-    internal class ExchangeRateAPI
+    public static class ExchangeRateAPI
     {
+        public static decimal ExchangeRate(string ID) //"R01235" USD; "R01239" EUR
+        {
+            var reader = new XmlTextReader("http://www.cbr.ru/scripts/XML_daily.asp");
+            var ExchangeXml = "";
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+
+                        if (reader.Name == "Valute")
+                        {
+                            if (reader.HasAttributes)
+                            {
+                                while (reader.MoveToNextAttribute())
+                                {
+                                    if (reader.Name == "ID")
+                                    {
+                                        if (reader.Value == ID)
+                                        {
+                                            reader.MoveToElement();
+                                            ExchangeXml = reader.ReadOuterXml();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+            XmlDocument XmlDocument = new XmlDocument();
+            XmlDocument.LoadXml(ExchangeXml);
+            XmlNode xmlNode = XmlDocument.SelectSingleNode("Valute/Value");
+            decimal rate = Convert.ToDecimal(xmlNode.InnerText);
+            return rate;
+        }
     }
 }
