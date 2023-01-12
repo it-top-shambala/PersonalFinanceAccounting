9b7f0b7060fe612827f6f03025a667f63d0d80bc
evgsir@gmail.com <evgsir@gmail.com>
2023-01-12 21:00:44 +0500

    Добалено создание таблицы расходов.

--- SQL/CreateDB.sql
+++ SQL/CreateDB.sql
@@ -9,7 +9,7 @@ CREATE TABLE tab_category_refill(
     name TEXT
 );
 
-CREATE TABLE tab_category_expence(
+CREATE TABLE tab_category_expense(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT
 );
@@ -29,6 +29,16 @@ CREATE TABLE tab_reffiling(
     id_category_refill INTEGER,
     sum INTEGER,
     FOREIGN KEY (id_category_refill)  REFERENCES tab_category_refill (id),
+    FOREIGN KEY (id_wallet)  REFERENCES tab_wallets (id)
+);
+
+CREATE TABLE tab_expensing(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    date_Time TEXT,
+    id_wallet INTEGER,
+    id_category_expense INTEGER,
+    sum INTEGER,
+    FOREIGN KEY (id_category_expense)  REFERENCES tab_category_expense (id),
     FOREIGN KEY (id_wallet)  REFERENCES tab_wallets (id)
 );
 
