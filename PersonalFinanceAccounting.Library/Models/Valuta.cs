706b5cc594f75bbf418f493c653681ad11d418eb
Starinin Andrey <starinin-andrey@ya.ru>
2023-01-12 23:12:34 +0300

    Удаление файлов из-за ошибочного слияния веток

--- PersonalFinanceAccounting.Library/Models/Valuta.cs
+++
@@ -1,9 +0,0 @@
-namespace PersonalFinanceAccounting.Library.Models
-{
-    internal class Valuta : Notify
-    {
-        public int Id { get; set; }
-        public string? NameValuta { get; set; }
-        public int CodeValuta { get; set; }
-    }
-}
