de0851293a30be2e2eb7a981b3b474b92edae0bd
evgsir@gmail.com <evgsir@gmail.com>
2023-01-16 17:09:29 +0500

    Приведение именования таблиц к указаной в документации. Добавление правил Update и Delete.

--- SQL/CreateDB.sql
+++ SQL/CreateDB.sql
@@ -1,44 +1,54 @@
-CREATE TABLE tab_valuta(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name TEXT,
-    code TEXT
+CREATE TABLE tab_currency(
+    currency_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL,
+    code TEXT NOT NULL
 );
 
-CREATE TABLE tab_category_refill(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name TEXT
+CREATE TABLE tab_category_income(
+    category_income_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE tab_category_expense(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name TEXT
+    category_expense_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE tab_wallets(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name TEXT,
-    amount REAL,
-    id_valuta INTEGER,
-    FOREIGN KEY (id_valuta)  REFERENCES tab_valuta (id)
+    wallet_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL,
+    balance REAL NULL,
+    currency_id INTEGER NOT NULL,
+    FOREIGN KEY (currency_id) REFERENCES tab_currency (currency_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 );
 
-CREATE TABLE tab_reffiling(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    date_Time TEXT,
-    id_wallet INTEGER,
-    id_category_refill INTEGER,
-    sum INTEGER,
-    FOREIGN KEY (id_category_refill)  REFERENCES tab_category_refill (id),
-    FOREIGN KEY (id_wallet)  REFERENCES tab_wallets (id)
+CREATE TABLE tab_incoming(
+    income_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    date_Time TEXT NOT NULL,
+    wallet_id INTEGER NOT NULL,
+    category_income_id INTEGER NOT NULL,
+    summa REAL NOT NULL,
+    FOREIGN KEY (income_id)  REFERENCES tab_category_income (category_income_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    FOREIGN KEY (wallet_id)  REFERENCES tab_wallets (wallet_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 );
 
 CREATE TABLE tab_expensing(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    date_Time TEXT,
-    id_wallet INTEGER,
-    id_category_expense INTEGER,
-    sum INTEGER,
-    FOREIGN KEY (id_category_expense)  REFERENCES tab_category_expense (id),
-    FOREIGN KEY (id_wallet)  REFERENCES tab_wallets (id)
+    expense_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    date_time TEXT NOT NULL,
+    wallet_id INTEGER NOT NULL,
+    category_expense_id INTEGER NOT NULL,
+    summa INTEGER NOT NULL,
+    FOREIGN KEY (category_expense_id)  REFERENCES tab_category_expense (category_expense_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    FOREIGN KEY (wallet_id)  REFERENCES tab_wallets (wallet_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 );
 
